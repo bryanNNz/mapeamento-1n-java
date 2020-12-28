dfc032a53ee4b8192e80d296d83b85ed487686f4
bryanNNz <bryanlukas1997@gmail.com>
2020-12-28 15:34:37 -0300

    Commit inicial

---
+++ src/main/java/com/mapeamento/Mapeamento1nApplication.java
@@ -0,0 +1,13 @@
+package com.mapeamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mapeamento1nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mapeamento1nApplication.class, args);
+	}
+
+}
