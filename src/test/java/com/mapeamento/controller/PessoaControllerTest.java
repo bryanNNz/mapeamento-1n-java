7fdda8a64e4fee8a666f47334f61d14821e1c082
bryanNNz <bryanlukas1997@gmail.com>
2021-01-24 12:21:53 -0300

    Adiciona testes em PessoaControllerTest

--- src/test/java/com/mapeamento/controller/PessoaControllerTest.java
+++ src/test/java/com/mapeamento/controller/PessoaControllerTest.java
@@ -42,15 +42,16 @@ public class PessoaControllerTest {
 	@Test
 	public void deve_retornar_status_200_quando_existir_registro() throws Exception {
 		//CENARIO
-		doReturn(Arrays.asList(PessoaBuilder.builder().id(1L).nome("MARIA").get()))
+		Pessoa pessoa = PessoaBuilder.builder().id(1L).nome("MARIA").get();
+		doReturn(Arrays.asList(pessoa))
 			.when(pessoaService).getAll();
 		
 		//ACAO/VERIFICACAO
 		mockMvc.perform(get("/pessoas")
 				.accept(MediaType.APPLICATION_JSON_VALUE))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.[0].id", is(equalTo(1))))
-			.andExpect(jsonPath("$.[0].nome", is(equalTo("MARIA"))));
+			.andExpect(jsonPath("$.[0].id", is(equalTo(pessoa.getId().intValue()))))
+			.andExpect(jsonPath("$.[0].nome", is(equalTo(pessoa.getNome()))));
 	}
 	
 	@Test
@@ -62,5 +63,30 @@ public class PessoaControllerTest {
 		//ACAO/VERIFICACAO
 		mockMvc.perform(get("/pessoas"))
 			.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void deve_retornar_status_200_quando_existir_registro_por_id() throws Exception {
+		//CENARIO
+		Pessoa pessoa = PessoaBuilder.builder().id(1L).nome("MARIA").get();
+		doReturn(pessoa)
+			.when(pessoaService).getById(pessoa.getId());
+		
+		//ACAO/VERIFICACAO
+		mockMvc.perform(get("/pessoa/{id}", 1L))
+			.andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	public void deve_retornar_status_404_quando_nao_existir_registro_por_id() throws Exception {
+		//CENARIO
+		Pessoa pessoa = PessoaBuilder.builder().id(1L).nome("JOAO").get();
+		doReturn(pessoa)
+			.when(pessoaService).getById(pessoa.getId());
+		
+		//ACAO/VERIFICACAO
+		mockMvc.perform(get("/pessoa/{id}", 2L))
+			.andExpect(status().isNotFound());
 	}
 }
